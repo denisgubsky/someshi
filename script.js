9f1a6e47c9ff99ef2ad10b2f77e0e2f5e9b9683e
denisgubsky <denisgubsky122333@gmail.com>
2025-07-01 20:43:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const calculator = {
   read() {
     this.a = Number(prompt('enter first num please', 0));
-
     if (isNaN(this.a)) {
       alert('Enter a NUMBER, please.');
       return this.read();
     }
-
     this.b = Number(prompt('enter second num please', 0));
-
     if (isNaN(this.b)) {
       alert('Enter a NUMBER, please.');
       return this.read();
@@ -25,3 +22,25 @@ const calculator = {
 calculator.read();
 alert(calculator.sum());
 alert(calculator.mul());
+
+
+//
+
+
+const ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    console.log(this.step);
+    return this;
+  },
+};
+
+ladder.up().down().up().up().showStep();
