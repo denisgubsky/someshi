b305cf95e85557614f6318311e1fbfdab18df6ba
denisgubsky <denisgubsky122333@gmail.com>
2025-07-09 13:46:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-const calculator = {
-  read() {
-    this.a = Number(prompt('enter first num please', 0));
-    if (isNaN(this.a)) {
-      alert('Enter a NUMBER, please.');
-      return this.read();
-    }
-    this.b = Number(prompt('enter second num please', 0));
-    if (isNaN(this.b)) {
-      alert('Enter a NUMBER, please.');
-      return this.read();
-    }
-  },
-  sum() {
-    return this.a + this.b;
-  },
-  mul() {
-    return this.a * this.b;
-  },
-};
+class AllShi {
+  static calculator = {
+    read() {
+      this.a = Number(prompt('enter first num please', 0));
+      if (isNaN(this.a)) {
+        alert('Enter a NUMBER, please.');
+        return this.read();
+      }
+      this.b = Number(prompt('enter second num please', 0));
+      if (isNaN(this.b)) {
+        alert('Enter a NUMBER, please.');
+        return this.read();
+      }
+    },
+    sum() {
+      return this.a + this.b;
+    },
+    mul() {
+      return this.a * this.b;
+    },
+  };
+}
 
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
+AllShi.calculator.read();
+alert(AllShi.calculator.sum());
+alert(AllShi.calculator.mul());
 
 
 //
